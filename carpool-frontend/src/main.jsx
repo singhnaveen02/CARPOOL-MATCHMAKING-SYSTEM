173a1a25562cd55decbfd7c8dd55c1c5baf65c33
singhnaveen02 <singhnaveen0022@gmail.com>
2026-06-13 01:22:38 +0530

    updated

--- carpool-frontend/src/main.jsx
+++ carpool-frontend/src/main.jsx
@@ -1,35 +1,9 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Navbar from './components/Common/Navbar';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import ProfilePage from './pages/ProfilePage';
-import DashboardPage from './pages/DashboardPage';
-import SearchPage from './pages/SearchPage';
-import AuthContext from './context/AuthContext';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-function App() {
-  return (
-    <AuthContext>
-      <BrowserRouter>
-        <div className="min-h-screen bg-gray-50">
-          <Navbar />
-          <main className="max-w-7xl mx-auto px-4 py-8">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/signup" element={<SignupPage />} />
-              <Route path="/profile" element={<ProfilePage />} />
-              <Route path="/dashboard" element={<DashboardPage />} />
-              <Route path="/search" element={<SearchPage />} />
-              <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-          </main>
-        </div>
-      </BrowserRouter>
-    </AuthContext>
-  );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
